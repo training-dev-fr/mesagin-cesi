aa3324cf82c62629bd3fb28ed3161e39febe1e11
training-dev <contact@training-dev.fr>
2026-05-13 10:04:28 +0200

    init user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sendme.Models;
+
+namespace sendme.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private List<UserModel> listUser = new List<UserModel>();
+
+        [HttpPost]
+        [Route("register")]
+        public ActionResult Register()
+        {
+            Console.WriteLine("Register");
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public ActionResult Login()
+        {
+            Console.WriteLine("Login");
+            return Ok();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace sendme.Models
+{
+    public class UserModel
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+        public UserModel(string? name, string? email, string? password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
